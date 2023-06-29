0237daef04124a554bda34af050bf53a4eb36e7a
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-29 15:13:15 +0300

    Update replace_codeuri.sh

--- replace_codeuri.sh
+++ replace_codeuri.sh
@@ -20,10 +20,10 @@ if grep -q "CustomResourceLambdaTriggerFunction" "$template_file"; then
     sed -i '' "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $template_file
 fi
 
-sed -i '' '1s/^/#created automatically from coralogix\/coralogix-aws-serverless\n/' $template_file
+# sed -i '' '1s/^/#created automatically from coralogix\/coralogix-aws-serverless\n/' $template_file
 
 # sed -i '' '/^## AWS Resource Manager Template Deployment$/,/^## Fields/c\
 # ## Fields 
 # ' $README_file
 
-sed -i '' '3s/^/This template were created automatically from coralogix\/coralogix-aws-serverless.\nTo make a change in the template go to the serverless repo and change he file from there.\n\n/' $README_file
+# sed -i '' '3s/^/This template were created automatically from coralogix\/coralogix-aws-serverless.\nTo make a change in the template go to the serverless repo and change he file from there.\n\n/' $README_file
