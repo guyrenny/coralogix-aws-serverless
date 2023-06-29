e968cc94e8618a1c29ca3ebfdd014501f84f30e3
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-29 11:24:55 +0300

    Update replace_codeuri.sh

--- replace_codeuri.sh
+++ replace_codeuri.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
-apt-get install moreutils -y
+# This script will change the codeUri in a given template, so that the template will be validate
+# for cloudformation.
 
 file=$1
+puckage_name=$2
+bucket_string="!Sub coralogix-serverless-repo-\${AWS::Region}"
 
-declare -i lambda_counter=0
-declare -i CustomResourceLambdaTriggerFunction_counter=0
 if grep -q "LambdaFunction" "$file"; then
-    let lambda_counter++
+    yq eval '.Resources.LambdaFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
 if grep -q "LambdaFunctionSSM" "$file"; then
-    let lambda_counter++
+    yq eval '.Resources.LambdaFunctionSSM.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
 if grep -q "CustomResourceLambdaTriggerFunction" "$file"; then
-    let CustomResourceLambdaTriggerFunction_counter++
-fi
-echo $lambda_counter
-echo $CustomResourceLambdaTriggerFunction_counter
-
-lambda_replacement="CodeUri: \n        Bucket: !Sub \'coralogix-serverless-repo-\${AWS::Region}\' \n        S3Key: \'cloudtrail.zip\'"
-custom_resource_replacement="CodeUri: \n        Bucket: !Sub \'coralogix-serverless-repo-\${AWS::Region}\' \n        S3Key: \'helper.zip\'"
-
-if [[ $CustomResourceLambdaTriggerFunction_counter -eq 0 ]]; then
-    sed -i.bak "s/CodeUri: \./$lambda_replacement/g" "$file"
-
-elif [[ $lambda_counter -eq 2 ]] && [[ $CustomResourceLambdaTriggerFunction_counter -eq 1 ]]; then
-    tr '\n' '@' < "$file" | sed "s/CodeUri: \./$lambda_replacement/1" | tr '@' '\n' | sponge "$file"
-    tr '\n' '@' < "$file" | sed "s/CodeUri: \./$lambda_replacement/1" | tr '@' '\n' | sponge "$file"
-    tr '\n' '@' < "$file" | sed "s/CodeUri: \./$custom_resource_replacement/1" | tr '@' '\n' | sponge "$file"
-
-elif [[ $lambda_counter -eq 1 ]] && [[ $CustomResourceLambdaTriggerFunction_counter -eq 1 ]]; then
-    tr '\n' '@' < "$file" | sed "s/CodeUri: \./$lambda_replacement/1" | tr '@' '\n' | sponge "$file"
-    tr '\n' '@' < "$file" | sed "s/CodeUri: \./$custom_resource_replacement/1" | tr '@' '\n' | sponge "$file"
+    yq eval '.Resources.CustomResourceLambdaTriggerFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "helper.zip"}' -i $file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
