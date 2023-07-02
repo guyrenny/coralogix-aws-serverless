a177c64813006a578b0b25b57f4a1497db47ff47
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-07-02 15:18:49 +0300

    Update replace_codeuri.sh

--- replace_codeuri.sh
+++ replace_codeuri.sh
@@ -7,15 +7,15 @@ puckage_name=$2
 bucket_string="!Sub coralogix-serverless-repo-\${AWS::Region}"
 
 if grep -q "LambdaFunction" "$file"; then
-    yq eval '.Resources.LambdaFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $file
+    yq eval '.Resources.LambdaFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "Key": "'"$puckage_name"'.zip"}' -i $file
     sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
 if grep -q "LambdaFunctionSSM" "$file"; then
-    yq eval '.Resources.LambdaFunctionSSM.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $file
+    yq eval '.Resources.LambdaFunctionSSM.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "Key": "'"$puckage_name"'.zip"}' -i $file
     sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
 if grep -q "CustomResourceLambdaTriggerFunction" "$file"; then
-    yq eval '.Resources.CustomResourceLambdaTriggerFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "helper.zip"}' -i $file
+    yq eval '.Resources.CustomResourceLambdaTriggerFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "Key": "helper.zip"}' -i $file
     sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $file
 fi
 
